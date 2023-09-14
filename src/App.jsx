c9ea86e1f2b4d4aa566ebdacef04147901745f88
makamnetz <144923469+makamnetz@users.noreply.github.com>
2023-09-14 10:37:48 +0300

    update route

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ function App() {
               }
             />
             <Route
-              path='/makam'
+              path='/makam/*'
               element={
                 <MakamScreen handleScreen={handleScreen} screen={screen} />
               }
