1d17b910439c04af77cdd8bcf59b7ff525ce5788
gumrahsindar <gsindar@gmail.com>
2023-10-11 19:04:14 +0300

    add mobile navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,22 +5,30 @@ import Navbar from 'react-bootstrap/Navbar'
 function AhenkNavbar() {
   return (
     <>
-      <Navbar className='w-75 mx-auto border-0' bg='dark' variant='dark'>
+      <Navbar
+        className='w-75 mx-auto border-0'
+        bg='dark'
+        expand='md'
+        variant='dark'
+      >
         <Container fluid>
           <Navbar.Brand className='border-0 fs-2' href='/'>
             MakamNetz
           </Navbar.Brand>
-          <Nav>
-            <Nav.Link href='/' className='border-0'>
-              Çesni Space
-            </Nav.Link>
-            <Nav.Link href='/makam' className='border-0'>
-              Makams
-            </Nav.Link>
-            <Nav.Link href='/makam_network' className='border-0'>
-              Makam Network
-            </Nav.Link>
-          </Nav>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ms-auto'>
+              <Nav.Link href='/' className='border-0'>
+                Çesni Space
+              </Nav.Link>
+              <Nav.Link href='/makam' className='border-0'>
+                Makams
+              </Nav.Link>
+              <Nav.Link href='/makam_network' className='border-0'>
+                Makam Network
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </>
